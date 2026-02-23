031a39274ebd4c7e08e862d8faaf812cf720e428
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-02-23 15:10:41 +0000

    Refactor init function to improve loader visibility and clean up commented code

--- src/main.js
+++ src/main.js
@@ -1,18 +1,26 @@
 import { getAllRecipes } from "./api/recipeProvider.js";
 import { getColor, calculateTotalCalories } from './services/calorieService.js';
-
+import { showLoader, hideLoader }  from "./ui/loader.js";
 async function init() {
-     console.log("NutriFlow lancée ");
+    //  console.log("NutriFlow lancée ");
+try {
+    showLoader();
 
     const recipes = await getAllRecipes();
-    console.table(recipes)
-    
+    // console.table(recipes)
+    renderRecipes(recipes)
+}catch (error){
+    console.error("probleme in init: ", error);
+}finally{
+    hideLoader();
+}
+
 }   
 init();
 
 
-console.log("Couleur pour 350 kcal :", getColor(350)); 
-console.log("Couleur pour 900 kcal :", getColor(900)); 
+// console.log("Couleur pour 350 kcal :", getColor(350)); 
+// console.log("Couleur pour 900 kcal :", getColor(900)); 
 
-const mesFavoris = [{ caloriesPerServing: 200 }, { caloriesPerServing: 500 }];
-console.log("Total calories :", calculateTotalCalories(mesFavoris));
+// const mesFavoris = [{ caloriesPerServing: 200 }, { caloriesPerServing: 500 }];
+// console.log("Total calories :", calculateTotalCalories(mesFavoris));
